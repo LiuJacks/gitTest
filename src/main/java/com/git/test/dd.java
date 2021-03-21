520f9aa7cf3e0560c5bf7ce57156ab3c52d055f1
Liujacks <895271128@qq.com>
2021-03-21 22:04:38 +0800

    解决冲突

--- src/main/java/com/git/test/dd.java
+++ src/main/java/com/git/test/dd.java
@@ -1,15 +1,7 @@
 package com.git.test;
 
-<<<<<<< HEAD
-import jdk.nashorn.internal.runtime.regexp.joni.exception.SyntaxException;
-
 public class dd {
     public static void main(String[] args) {
         System.out.println("dd");
-=======
-public class dd {
-    public static void main(String[] args) {
-      System.out.println("this is dd");
->>>>>>> dev
     }
 }
