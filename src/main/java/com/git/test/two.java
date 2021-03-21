1b79af0d1405e17d07d66988e48e3802210ee72d
Liujacks <895271128@qq.com>
2021-03-21 20:58:49 +0800

    修改了two.java

--- src/main/java/com/git/test/two.java
+++ src/main/java/com/git/test/two.java
@@ -2,6 +2,6 @@ package com.git.test;
 
 public class two {
     public static void main(String[] args) {
-        System.out.println("this is two");
+        System.out.println("this is two，hi");
     }
 }
