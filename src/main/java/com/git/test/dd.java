25ef964388e1b78824cde6751a54ee14031ff0a4
Liujacks <895271128@qq.com>
2021-03-21 21:55:05 +0800

    dd新文件

--- src/main/java/com/git/test/dd.java
+++ src/main/java/com/git/test/dd.java
@@ -1,7 +1,7 @@
 package com.git.test;
 
-public class three {
+public class dd {
     public static void main(String[] args) {
-      System.out.println("this is thress");
+      System.out.println("this is dd");
     }
 }
